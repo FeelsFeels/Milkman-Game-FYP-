1cc4468d6c5a51bad0e5be6baa0a5a52a71f5945
Eeyn <euniceeyn@gmail.com>
2019-04-22 23:43:15 +0800

    Game manager
    Only basics

--- NewBeans Game/Assets/Scripts/GameManager.cs
+++ NewBeans Game/Assets/Scripts/GameManager.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager instance = null;
+    
+
+    // Awake is always called before any Start functions
+    void Awake()
+    {
+        // Check if instance already exists;
+        if (instance == null)
+        {
+            // If not, set instance to this.
+            instance = this;
+        }
+
+        // If instance already exists and is not this;
+        else if (instance != this)
+        {
+            // Then destroy this. (There can only be one instance of a GameManager).
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
