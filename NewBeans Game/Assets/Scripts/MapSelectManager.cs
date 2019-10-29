b24e085a2af6024c5730b860faeb8381c8eb7584
Eeyn <euniceeyn@gmail.com>
2019-10-29 13:49:26 +0800

    clean up MapSelectManager.cs

--- NewBeans Game/Assets/Scripts/MapSelectManager.cs
+++ NewBeans Game/Assets/Scripts/MapSelectManager.cs
@@ -11,19 +11,11 @@ public class MapSelectManager : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
-        // anim = GetComponent<Animation>();
         mapNumber = 1;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void RightButtonPressed()
     {
-        anim.speed = 1.0f;
         if (mapNumber == 1)
         {
             anim.Play("MapSelect_1_to_2");
@@ -50,21 +42,18 @@ public class MapSelectManager : MonoBehaviour
     {
         if (mapNumber == 2)
         {
-            
             anim.Play("MapSelect_2_to_1");
             mapNumber = 1;
             return;
         }
         if (mapNumber == 3)
         {
-
             anim.Play("MapSelect_3_to_2");
             mapNumber = 2;
             return;
         }
         if (mapNumber == 4)
         {
-
             anim.Play("MapSelect_4_to_3");
             mapNumber = 3;
             return;
