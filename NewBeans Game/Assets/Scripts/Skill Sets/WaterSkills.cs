4d68f9e29222def532009ae5999322153be4346d
Lily <theluckofthedrawer@gmail.com>
2019-11-25 13:56:16 +0800

    Tornadoes

---
+++ NewBeans Game/Assets/Scripts/Skill Sets/WaterSkills.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSkills : SkillSetManager.SkillSet
+{
+    public GameObject waterTornado;
+    public float timeForTornadoGrowth = 0.2f;
+    Transform skillUser;
+    WaterTornado storm;
+
+    public override void SkillAttack(SkillSetManager manager)
+    {
+        skillUser = manager.GetComponent<Transform>(); // Set the player
+        StartCoroutine(EndSkill(manager));
+
+        //Set where the tornado shld appear
+        // Set the pos to the front of the player
+        Vector3 pos = skillUser.position + skillUser.TransformDirection(skillUser.forward) * 5f + skillUser.TransformDirection(skillUser.up) * 0.1f;
+        Debug.DrawLine(pos, pos + Vector3.down * 10, Color.yellow, 5);
+        //Instantiate
+        WaterTornado tornado = Instantiate(waterTornado, pos, Quaternion.identity).GetComponent<WaterTornado>();
+        Debug.Log(tornado);
+
+        storm = tornado;
+        tornado.SetSkillUser(skillUser);
+        StartCoroutine(GrowTornado());
+
+    }
+    
+
+    IEnumerator GrowTornado()
+    {
+        yield return new WaitForSeconds(timeForTornadoGrowth);
+
+        //Ref to tornado, and make tornado move forward
+        storm.BrewStorm((skillUser.forward.normalized));
+    }
+
+    IEnumerator EndSkill(SkillSetManager manager)
+    {
+        Debug.Log("Tornado has stopped");
+        yield return new WaitForSeconds(skillDuration);
+
+        storm.DestroyStorm();
+        EndUltimate(manager);
+    }
+    //private void Update()
+    //{
+    //    if(skillUser != null)
+    //    Debug.Log(skillUser.position);
+    //}
+}
---
+++ NewBeans Game/Assets/Scripts/Skill Sets/WaterTornado.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTornado : MonoBehaviour
+{
+
+    public float speed;
+    Rigidbody rb;
+    Transform skillUser;
+    public float suctionForce = 1000f;
+    bool tornadoStarted = false;
+    public float tornadoRadius = 5f;
+    public LayerMask playerLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSkillUser(Transform user)
+    {
+        skillUser = user;
+        tornadoStarted = true;
+    }
+
+
+    public void BrewStorm(Vector3 dir)
+    {
+        //Move it forward
+        rb.velocity = dir * speed;
+        StartCoroutine(Storm());
+    }
+
+    // Note: Have just removed collider on this obj so no longer using this
+    //private void OnCollisionEnter(Collision collision)
+    //{
+        //    PlayerController player = collision.transform.GetComponent<PlayerController>();
+
+        //    if (player && collision.gameObject != skillUser)
+        //    {
+        //        Vector3 direction = collision.transform.position - transform.position;
+        //        direction = direction.normalized;
+
+        //        //Push player towards centre
+        //        player.GetComponent<Rigidbody>().AddForce(direction * suctionForce);
+
+        //    }
+
+
+        // IF under water
+     //   if (transform.position.y< -20f || collision.gameObject.tag == "Water")
+     //   {
+     //           DestroyStorm();
+     //   }
+     //}
+
+    IEnumerator Storm()
+    {
+        while (tornadoStarted) { 
+            List<Collider> colliders = new List<Collider>();
+            colliders.AddRange(Physics.OverlapSphere(transform.position, tornadoRadius, playerLayer));
+
+            if (colliders.Contains(skillUser.GetComponent<Collider>())) //If it includes this player,
+            {
+                colliders.Remove(skillUser.GetComponent<Collider>()); // remove
+            }
+
+            foreach (Collider collider in colliders)
+            {
+                //Debug.Log("Player found");
+                //Get the inwards direction
+                Vector3 direction =  transform.position - collider.transform.position;
+                direction = direction.normalized;
+
+                //Push player towards centre of tornado
+                collider.GetComponent<Rigidbody>().AddForce(direction * suctionForce);
+                //Debug.Log("Sucking");
+            }
+
+            yield return null;
+        }
+    }
+
+    public void DestroyStorm()
+    {
+        tornadoStarted = false;
+        Destroy(this.gameObject);
+    }
+
+
+}
