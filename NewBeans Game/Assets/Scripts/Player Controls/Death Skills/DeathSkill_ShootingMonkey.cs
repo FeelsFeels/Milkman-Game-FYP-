b32416be00c17e3a299a77e308cffcd37a633a07
Rocky Sutarius <rockysutarius@gmail.com>
2019-12-01 00:47:53 +0800

    shooting monkey

---
+++ NewBeans Game/Assets/Scripts/Player Controls/Death Skills/DeathSkill_ShootingMonkey.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathSkill_ShootingMonkey : BaseDeathSkill
+{
+    public GeneralMovement generalMovement;
+    public GeneralRotation generalRotation;
+
+
+    public GameObject aimingCanvas;
+    public Image aimingImage;
+    public Image chargingImage;
+    public float timeCharged;
+
+    public Rigidbody monkey;
+    public Transform shootOrigin;
+    public GameObject monkeyPellets;
+    public Transform groundCheckTransform;
+
+    public float timeSinceLastShot;
+    public float shootingTimeElapsed;
+
+    bool deactivated;
+    bool skillUsed;
+    bool onGround;
+
+    public override void OnFinishSkill()
+    {
+        if (!deactivated)
+        {
+            deactivated = true;
+            assignedController.FinishUsingSkill();
+            Destroy(gameObject);
+        }
+    }
+
+    public override void OnReady()
+    {
+        aimingCanvas.SetActive(true);
+        aimingImage.color = assignedPlayer.inputInfo.chosenCharacterData.characterColor;
+        monkey.useGravity = false;
+    }
+
+    public override void OnUse()
+    {
+        if (!skillUsed)
+        {
+            skillUsed = true;
+            chargingImage.fillAmount = 0;
+            monkey.useGravity = true;
+            StartCoroutine(MoveMonkeyDown());
+        }
+    }
+
+    private void Update()
+    {
+        if (assignedPlayer == null || assignedController == null || deactivated)
+            return;
+
+        if (!skillUsed)
+        {
+            timeCharged += Time.deltaTime;
+            chargingImage.fillAmount = timeCharged / 10f;
+            if (timeCharged >= 10f)
+            {
+                OnUse();
+            }
+
+            float moveVerticalAxis = Input.GetAxisRaw(VerticalInputAxis);
+            float moveHorizontalAxis = Input.GetAxisRaw(HorizontalInputAxis);
+            Vector3 input = new Vector3(moveHorizontalAxis, 0, -moveVerticalAxis);
+            Vector3 direction = input.normalized;
+
+            if (moveHorizontalAxis != 0 || moveVerticalAxis != 0)
+            {
+                generalMovement.Move(direction);
+            }
+        }
+
+        if (onGround)
+        {
+            //Turning the monkey and its shoot position
+
+            float moveVerticalAxis = Input.GetAxisRaw(VerticalInputAxis);
+            float moveHorizontalAxis = Input.GetAxisRaw(HorizontalInputAxis);
+            Vector3 input = new Vector3(moveHorizontalAxis, 0, -moveVerticalAxis);
+            Vector3 direction = input.normalized;
+            generalRotation.Rotate(direction);
+            //Auto Shoot
+            timeSinceLastShot += Time.deltaTime;
+            shootingTimeElapsed += Time.deltaTime;
+            if(timeSinceLastShot >= 0.25f)
+            {
+                DeathSkill_ShootingMonkeyProjectile projectile = Instantiate(monkeyPellets, shootOrigin.position, Quaternion.identity).GetComponent<DeathSkill_ShootingMonkeyProjectile>();
+                projectile.Shoot(shootOrigin.forward);
+                projectile.monkey = monkey.gameObject;
+                timeSinceLastShot = 0;
+            }
+            if(shootingTimeElapsed >= 5f)
+            {
+                OnFinishSkill();
+            }
+
+            chargingImage.fillAmount = shootingTimeElapsed / 5f;
+        }
+
+        if (Input.GetButtonDown(AButtonInput))
+        {
+            OnUse();
+        }
+    }
+
+    IEnumerator MoveMonkeyDown()
+    {
+        float timeElapsed = 0;
+
+        while (true)
+        {
+            timeElapsed += Time.deltaTime;
+            monkey.transform.Translate(Vector3.down * 2);
+
+            if(timeElapsed >= 10f)
+            {
+                //Assume the monkey fell into water liao
+                Destroy(gameObject);
+            }
+            //Check if touch ground. Then can start shooting
+            if(Physics.CheckSphere(groundCheckTransform.position, 1f, 1 << LayerMask.NameToLayer("Ground")))
+            {
+                onGround = true;
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        OnFinishSkill();
+    }
+}
--- NewBeans Game/Assets/Scripts/Player Controls/Death Skills/Targetting, Movement Systems/GeneralRotation.cs
+++ NewBeans Game/Assets/Scripts/Player Controls/Death Skills/Targetting, Movement Systems/GeneralRotation.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeneralMovement : MonoBehaviour
+public class GeneralRotation : MonoBehaviour
 {
     public float playerTurnSmoothing = 10f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-    public float moveRate = 10;  // units moved per second holding down move input
+
     float cameraRigRot;
 
     private void Awake()
@@ -17,13 +17,10 @@ public class GeneralMovement : MonoBehaviour
             cameraRigRot = camera.transform.rotation.eulerAngles.y;
     }
 
-    public void Move(Vector3 direction)
+    public void Rotate(Vector3 direction)
     {
-        // Movement based on camera's rotation at the start.
-        Vector3 velocity = Quaternion.AngleAxis(cameraRigRot, Vector3.up) * (direction * moveRate); //Multiply the direction by the camera rotation
-        Vector3 movement = velocity * Time.deltaTime;
-        transform.Translate(movement, Space.World);
-
+        if (direction == Vector3.zero)
+            return;
         Quaternion correctedQ;
         correctedQ = Quaternion.LookRotation(Quaternion.AngleAxis(cameraRigRot, Vector3.up) * direction); //Correct the rotation quaternion 
         transform.rotation = Quaternion.Slerp(transform.rotation, correctedQ, Time.deltaTime * playerTurnSmoothing); //Rotate the player, and LERP THE ROTATION... 
