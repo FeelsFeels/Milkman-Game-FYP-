e3748be94e082d71fb0d28891540fc60a8696711
Lily <theluckofthedrawer@gmail.com>
2020-01-20 07:48:48 +0800

    Leaderboard
    -fly in anim not working yet

---
+++ NewBeans Game/Assets/Scripts/Manager Classes/Results Screen/RankingPrefab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingPrefab : MonoBehaviour
+{
+    public Text playerName;
+    public Image rankPng;
+    public Image icon;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("FlyIn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
