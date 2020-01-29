caff63e81f17c5db9f20e29a7585e5785508b4ca
Rocky Sutarius <rockysutarius@gmail.com>
2020-01-29 16:06:12 +0800

    go bwah

---
+++ NewBeans Game/Assets/Prefabs/Death Skill Prefabs/SuperDeathHissatsuJutsuJohnathanOUCH.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperDeathHissatsuJustsus
+{
+    public class SuperDeathHissatsuJutsuJohnathanOUCH : MonoBehaviour
+    {
+        public AudioSource maleHissatsu;
+        public AudioSource femaleHissatsu;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad8))
+            {
+                maleHissatsu.Play();
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad9))
+            {
+                femaleHissatsu.Play();
+            }
+        }
+    }
+}
