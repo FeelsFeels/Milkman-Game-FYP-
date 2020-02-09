5f2b248d153a53bccff496448ae0f44008061d56
Eeyn <euniceeyn@gmail.com>
2020-02-09 21:47:34 +0800

    charging vfx improvements

--- NewBeans Game/Assets/RotateAroundZ.cs
+++ NewBeans Game/Assets/RotateAroundZ.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateAround : MonoBehaviour
+public class RotateAroundZ : MonoBehaviour
 {
     public float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
