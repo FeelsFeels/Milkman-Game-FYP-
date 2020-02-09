c77d38bf09c823f63d3987ee0db2b7bae8e431c2
Rocky Sutarius <rockysutarius@gmail.com>
2020-02-09 20:34:20 +0800

    Finished Instructions screen

---
+++ NewBeans Game/Assets/Scripts/AI Controlled/Shitty Hardcoded Instructions Screen/EarthUltimateSimulation.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NewBeans.InstructionsScreen
+{
+    public class EarthUltimateSimulation : BaseSimulation
+    {
+        public Transform player1ResetPos;
+        public Transform player2ResetPos;
+
+        public override void ResetSimulation()
+        {
+            StopAllCoroutines();
+            player1.ResetStates();
+            player2.ResetStates();
+            if (player1 && player1ResetPos)
+            {
+                player1.transform.position = player1ResetPos.position;
+                player1.transform.rotation = player1ResetPos.rotation;
+            }
+            if (player2 && player2ResetPos)
+            {
+                player2.transform.position = player2ResetPos.position;
+                player2.transform.rotation = player2ResetPos.rotation;
+            }
+        }
+
+        public override void StartSimulation()
+        {
+            ResetSimulation();
+            StartCoroutine("RulesSimulationRoutine1");
+            StartCoroutine("RulesSimulationRoutine2");
+            StartCoroutine("RulesSimulationRoutine3");
+        }
+
+        IEnumerator RulesSimulationRoutine1()
+        {
+            yield return new WaitForSeconds(0.5f);
+            player1.GetComponent<AIPlayerSkillSetManager>().currentCharge = 13195f;
+            player1.GetComponent<AIPlayerSkillSetManager>().ReleaseSpecialSkill();
+            yield return new WaitForSeconds(1.0f);
+            float timepassed = 0f;
+            while (timepassed < 5.0f)
+            {
+                timepassed += Time.deltaTime;
+                player1.Move(AIPlayerInputController.Direction.E);
+                yield return null;
+            }
+            player1.Move(AIPlayerInputController.Direction.Still);
+            yield return new WaitForSeconds(1.0f);
+            StartSimulation();
+        }
+        IEnumerator RulesSimulationRoutine2()
+        {
+            yield return new WaitForSeconds(0.5f);
+            for(int i = 0; i <= 5; i++)
+            {
+                player2.HoldShootButton();
+                yield return new WaitForSeconds(0.2f);
+                player2.ReleaseShootButton();
+                yield return new WaitForSeconds(0.7f);
+            }
+        }
+        IEnumerator RulesSimulationRoutine3()
+        {
+            yield return new WaitForSeconds(0.5f);
+            float timepassed = 0f;
+            while (timepassed < 5.0f)
+            {
+                Vector3 turnDirection = (player1.transform.position - player2.transform.position).normalized;
+                player2.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(turnDirection), 10);
+                yield return null;
+            }
+        }
+
+    }
+}
---
+++ NewBeans Game/Assets/Scripts/AI Controlled/Ulti Skills/AIEarthUlti.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NewBeans.InstructionsScreen
+{
+    public class AIEarthUlti : AIPlayerSkillSetManager.SkillSet
+    {
+        public GameObject shockwaveParticles;
+
+        public float timeToNextShockwave = 0f;
+        public float knockbackRadius;
+        public float knockbackStrength;
+        AIPlayerSkillSetManager skillManager;
+        Transform skillUser;
+        public bool startSmashing;
+        Rigidbody userRb;
+
+
+
+
+        public override void SetManagerAndCharacter(AIPlayerSkillSetManager player)
+        {
+            skillManager = player;
+            skillUser = player.transform;
+        }
+
+        public override void SkillAttack(AIPlayerSkillSetManager manager)
+        {
+            skillUser = manager.GetComponent<Transform>(); // Set the player
+            startSmashing = true;
+
+            // Size up player
+            skillUser.localScale *= 3;
+            //Add weight
+            userRb = skillUser.GetComponent<Rigidbody>();
+            userRb.mass = 20;
+
+            StartCoroutine(EndSkill(manager));
+            StartCoroutine(UpdatePlayerBehaviour());
+
+        }
+
+        IEnumerator UpdatePlayerBehaviour()
+        {
+            while (startSmashing) //Keep updating while this is true
+            {
+                //Disable player getting stunned, let's try let's go
+                if (skillUser.GetComponent<AIPlayerController>().playerStunned)
+                {
+                    AIPlayerController player = skillUser.GetComponent<AIPlayerController>();
+                    player.playerStunned = false;
+                    player.stunnedTime = 0;
+                    player.dizzyStars.SetActive(false);
+                }
+
+                timeToNextShockwave += Time.deltaTime;
+
+                if (timeToNextShockwave >= 0.666f)
+                {
+                    timeToNextShockwave = 0;
+                    Shockwave();
+                }
+
+                yield return null;
+            }
+
+        }
+
+        void Shockwave()
+        {
+            AutoDestroyOverTime particles = Instantiate(shockwaveParticles, skillUser.position, shockwaveParticles.transform.rotation).GetComponent<AutoDestroyOverTime>();
+            particles.DestroyWithTime(0.3f);
+
+            //Gets all players in range of shockwave stomp
+            int ignoreLayerMask = ~1 << LayerMask.NameToLayer("Ground");    //Raycasts on everything but ground
+
+            List<Collider> inRange = new List<Collider>();
+            inRange.AddRange(Physics.OverlapSphere(skillUser.position, knockbackRadius, ignoreLayerMask));
+            if (inRange.Contains(skillUser.GetComponent<Collider>())) //If it includes this player,
+            {
+                inRange.Remove(skillUser.GetComponent<Collider>()); // remove
+
+            }
+
+            //Disrupts all players in range
+            foreach (Collider collider in inRange)
+            {
+                AIPlayerController player = collider.GetComponent<AIPlayerController>();
+                if (player)
+                {
+                    Vector3 knockbackDirection = (player.transform.position - skillUser.position).normalized;
+                    player.GetComponent<Rigidbody>().AddForce(knockbackStrength * knockbackDirection);
+
+                    player.Hit();
+                }
+                else if (collider.tag == "Rock")
+                {
+                    Vector3 knockbackDirection = (collider.transform.position - skillUser.position).normalized;
+                    collider.GetComponent<HazardBoulder>().rb.AddForce(knockbackStrength * knockbackDirection);
+                }
+            }
+        }
+
+
+        IEnumerator EndSkill(AIPlayerSkillSetManager manager)
+        {
+            yield return new WaitForSeconds(skillDuration);
+
+            EndUltimate(manager);
+        }
+        public override void EndUltimate(AIPlayerSkillSetManager playerSkillManager)
+        {
+            base.EndUltimate(playerSkillManager);
+            StopAllCoroutines();
+            //Stop the smash
+            startSmashing = false;
+
+            //Size down player
+            playerSkillManager.transform.localScale = Vector3.one;
+            //Resume normal mass
+            if (userRb)
+            {
+                userRb.mass = 1;
+            }
+        }
+    }
+}
