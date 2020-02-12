366cc125bba1a735f94a2c1406ed71ad6ef7225f
Rocky Sutarius <rockysutarius@gmail.com>
2020-02-13 00:55:35 +0800

    Player Select v2 Complete

--- NewBeans Game/Assets/Scripts/Manager Classes/Player Select v2/PlayerSelectBehaviour.cs
+++ NewBeans Game/Assets/Scripts/Manager Classes/Player Select v2/PlayerSelectBehaviour.cs
@@ -15,11 +15,20 @@ public class PlayerSelectBehaviour : MonoBehaviour
     public PlayerInputInfo[] playerInfoArray = new PlayerInputInfo[4];      //Player Input scriptable object
     public CharacterData[] characterDataArray = new CharacterData[4];       //Specific character data scriptable objects
     public PlayerSelectCharacterSelector[] characterSelection = new PlayerSelectCharacterSelector[4];
+
+    public GameObject[] pressToJoinGame = new GameObject[4];
+    public Image[] selectionBorders = new Image[4];
+    public Image[] selectionOverlays = new Image[4];
+    public Image[] playerNumsImage = new Image[16];
+    public Image[] playerChosenCharPortraits = new Image[16];
+    public GameObject[] playerChosenCharNames = new GameObject[16];
+
+    public Dictionary<PlayerSelectCharacterSelector, int> selectorHoverOver = new Dictionary<PlayerSelectCharacterSelector, int>();
     public Dictionary<PlayerSelectCharacterSelector, int> playerCharacterChoices = new Dictionary<PlayerSelectCharacterSelector, int>();
 
     public List<int> playersJoined = new List<int>();   //Player numbers
 
-    bool canStartGame;
+    public bool canStartGame;
 
     public string SceneToLoad;
 
@@ -32,6 +41,20 @@ public class PlayerSelectBehaviour : MonoBehaviour
             player.chosenCharacterData = null;
             player.chosenCharacterIndex = 0;
             player.forceActive = false;
+        }
+
+        //Disables all graphics
+        for (int i = 0; i < 4; i++)
+        {
+            pressToJoinGame[i].SetActive(true);
+            selectionBorders[i].GetComponent<Image>().enabled = false;
+            selectionOverlays[i].GetComponent<Image>().enabled = false;
+        }
+        for (int i = 0; i < 16; i++)
+        {
+            playerNumsImage[i].GetComponent<Image>().enabled = false;
+            playerChosenCharPortraits[i].GetComponent<Image>().enabled = false;
+            playerChosenCharNames[i].SetActive(false);
         }
     }
 
@@ -67,15 +90,48 @@ public class PlayerSelectBehaviour : MonoBehaviour
         }
     }
 
+
+
+    public void UpdateUI()
+    {
+        int numPlayers = playersJoined.Count;
+
+
+        //This block disables all the selection borders
+        for (int i = 0; i < 4; i++)
+        {
+            selectionBorders[i].enabled = false;
+        }
+
+        //This block sets the player number indicator and border
+        for (int i = 0; i < numPlayers; i++)
+        {
+            //Disabling all graphics
+            for (int p = 0; p < 4; p++)
+            {
+                playerNumsImage[(i * 4) + p].enabled = false;
+            }
+
+            int hoveredChar = characterSelection[i].charHoverOverIndex;
+            //enables the player selection border
+            selectionBorders[hoveredChar].enabled = true;
+            //enables the number
+            playerNumsImage[(i * 4) + hoveredChar].enabled = true;
+        }
+    }
+
     void AddNewPlayer(int controllerNumber)
     {
+        print("New player has arrived! Controller " + controllerNumber);
         int latestPlayer = playersJoined.Count - 1;    //Making controller number and playernumber unrelated.
 
-        //pressToJoinSprites[latestPlayer].enabled = false;
+        pressToJoinGame[latestPlayer].SetActive(false);
 
         characterSelection[latestPlayer].PlayerJoined(playerInfoArray[latestPlayer]);
+        characterSelection[latestPlayer].charHoverOverIndex = 0;
         playerInfoArray[latestPlayer].SetInputStrings(controllerNumber);
         playerInfoArray[latestPlayer].chosenCharacterIndex = 0; //Setting as default character selection
+        UpdateUI();
         CheckIfCanStartGame();
     }
 
@@ -84,17 +140,30 @@ public class PlayerSelectBehaviour : MonoBehaviour
 
     }
 
-    public bool ChooseCharacter(PlayerInputInfo playerToChange, int characterIndex)
+    public void ChooseCharacter(PlayerInputInfo playerToChange, int characterIndex)
     {
+        playerToChange.chosenCharacterIndex = characterIndex;
+        playerToChange.chosenCharacterData = characterDataArray[characterIndex];
 
+        //print(characterIndex);
+        selectionOverlays[characterIndex].enabled = true;
+        //print(selectionOverlays[characterIndex].name);
+        //print(((playerToChange.playerNumber - 1) * 4) + characterIndex);
+        playerChosenCharPortraits[(playerToChange.playerNumber - 1) * 4 + characterIndex].enabled = true;
+        playerChosenCharNames[(playerToChange.playerNumber - 1) * 4 + characterIndex].SetActive(true);
+        UpdateUI();
         CheckIfCanStartGame();
-        return true;
     }
 
     public void UnchooseCharacter(PlayerInputInfo playerToChange, int characterIndex)
     {
+        playerToChange.chosenCharacterData = null;
 
+        selectionOverlays[characterIndex].enabled = false;
+        playerChosenCharPortraits[(playerToChange.playerNumber - 1) * 4 + characterIndex].enabled = false;
+        playerChosenCharNames[(playerToChange.playerNumber - 1) * 4 + characterIndex].SetActive(false);
 
+        UpdateUI();
         CheckIfCanStartGame();
     }
 
@@ -104,7 +173,8 @@ public class PlayerSelectBehaviour : MonoBehaviour
         bool characterTaken = false;
 
         if (playersJoined.Count < 2)
-            return true;
+            return false;
+
 
         for (int i = 0; i < playersJoined.Count; i++)
         {
@@ -115,25 +185,26 @@ public class PlayerSelectBehaviour : MonoBehaviour
             int chosenCharIndex = playerInfoArray[i].chosenCharacterIndex;
             if (chosenCharIndex == characterIndex)
             {
+                print("Taken: " + chosenCharIndex + " by player:" + i);
                 characterTaken = true;
                 break;
             }
             else
             {
+                //print("character not taken, next loop");
                 characterTaken = false;
                 continue;
             }
         }
 
         if (characterTaken)
-            return false;
-        else
             return true;
+        else
+            return false;
     }
 
     void CheckIfCanStartGame()
     {
-
         //Must be more than one player
         if (playersJoined.Count < 2)
             return;
--- NewBeans Game/Assets/Scripts/Manager Classes/Player Select v2/PlayerSelectCharacterSelector.cs
+++ NewBeans Game/Assets/Scripts/Manager Classes/Player Select v2/PlayerSelectCharacterSelector.cs
@@ -7,18 +7,24 @@ public class PlayerSelectCharacterSelector : MonoBehaviour
     public PlayerSelectBehaviour playerSelectBehaviour;
     public PlayerInputInfo playerInfo;
 
+    public bool activated;
+    public int charHoverOverIndex;
+    public bool chosenCharacter;
+
     bool horizontalHeld;
     bool verticalHeld;
 
     private void Awake()
     {
         playerSelectBehaviour = FindObjectOfType<PlayerSelectBehaviour>();
+        charHoverOverIndex = 0;
     }
-
+    
     private void Update()
     {
-        if (ReferenceEquals(playerInfo, null))
+        if (!activated)
             return;
+        
 
         float horizontal = Input.GetAxis(playerInfo.HorizontalInputAxis);
         float vertical = Input.GetAxis(playerInfo.VerticalInputAxis);
@@ -26,29 +32,38 @@ public class PlayerSelectCharacterSelector : MonoBehaviour
         //Horizontal movement
         if(horizontalHeld == false)
         {
-            if(horizontal >= 0.5f)
+            if (playerInfo.chosenCharacterData == null)
             {
-                MoveSelectionRight();
-            }
-            if(horizontal <= 0.5f)
-            {
-                MoveSelectionLeft();
+                if (horizontal >= 0.5f)
+                {
+                    horizontalHeld = true;
+                    MoveSelectionRight();
+                }
+                if (horizontal <= -0.5f)
+                {
+                    horizontalHeld = true;
+                    MoveSelectionLeft();
+                }
             }
         }
 
         //Vertical movement
         if (verticalHeld == false)
         {
-            if (vertical >= 0.5f)
+            if (playerInfo.chosenCharacterData == null)
             {
-                MoveSelectionUp();
-            }
-            if (vertical <= 0.5f)
-            {
-                MoveSelectionDown();
+                if (vertical >= 0.5f)
+                {
+                    verticalHeld = true;
+                    MoveSelectionDown();
+                }
+                if (vertical <= -0.5f)
+                {
+                    verticalHeld = true;
+                    MoveSelectionUp();
+                }
             }
         }
-
 
         if (horizontal == 0)
         {
@@ -59,27 +74,88 @@ public class PlayerSelectCharacterSelector : MonoBehaviour
         {
             verticalHeld = false;
         }
+
+        if(!horizontalHeld && !verticalHeld)
+        {
+            if (Input.GetButtonDown(playerInfo.AButtonInput))
+            {
+                SelectCharacter();
+            }
+            if (Input.GetButtonDown(playerInfo.BButtonInput))
+            {
+                DeselectCharacter();
+            }
+        }
     }
 
     public void PlayerJoined(PlayerInputInfo player)
     {
-        playerInfo = player;
+        StartCoroutine("ActivateInHalfSec");
     }
 
     void MoveSelectionRight()
     {
-
+        if (charHoverOverIndex != 1 && charHoverOverIndex != 3)
+        {
+            //print("RIGHT");
+            charHoverOverIndex++;
+            playerSelectBehaviour.UpdateUI();
+        }
     }
     void MoveSelectionLeft()
     {
+        if (charHoverOverIndex != 0 && charHoverOverIndex != 2)
+        {
+            //print("LEFT");
+            charHoverOverIndex--;
+            playerSelectBehaviour.UpdateUI();
+        }
 
     }
     void MoveSelectionUp()
     {
+        if (charHoverOverIndex != 0 && charHoverOverIndex != 1)
+        {
+            //print("UP");
+            charHoverOverIndex -= 2;
+            playerSelectBehaviour.UpdateUI();
+        }
 
     }
     void MoveSelectionDown()
     {
+        if (charHoverOverIndex != 2 && charHoverOverIndex != 3)
+        {
+            //print("DOWN");
+            charHoverOverIndex += 2;
+            playerSelectBehaviour.UpdateUI();
+        }
+    }
 
+    void SelectCharacter()
+    {
+        if (playerInfo.chosenCharacterData != null)
+            return;
+
+        //print("Is character taken? Answer: " + playerSelectBehaviour.CheckIfCharacterTaken(playerInfo, charHoverOverIndex));
+        if (!playerSelectBehaviour.CheckIfCharacterTaken(playerInfo, charHoverOverIndex))
+        {
+            //print("selection");
+            playerSelectBehaviour.ChooseCharacter(playerInfo, charHoverOverIndex);
+        }
+    }
+    void DeselectCharacter()
+    {
+        if (playerInfo.chosenCharacterData != null)
+        {
+            //print("deselection");
+            playerSelectBehaviour.UnchooseCharacter(playerInfo, charHoverOverIndex);
+        }
+    }
+
+    IEnumerator ActivateInHalfSec()
+    {
+        yield return new WaitForSeconds(0.2f);
+        activated = true;
     }
 }
