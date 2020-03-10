985539e3904fbadd90d22d5a0ef98af714b78277
Rocky Sutarius <rockysutarius@gmail.com>
2020-03-10 17:21:17 +0800

    added water splash sound

---
+++ NewBeans Game/Assets/Scripts/Helper Scripts/PlaySoundOnContact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnContact : MonoBehaviour
+{
+    AudioManager audioManager;
+
+    [SerializeField]
+    string audioString;
+
+    private void Awake()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (audioManager)
+                audioManager.Play(audioString);
+        }
+    }
+}
